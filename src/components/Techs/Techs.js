6b0bffcb18879fe079eeb1d1f3bb3c4516b35e2b
Anna Kozhevatova <ad.kozhevatova@yandex.ru>
2021-03-15 11:45:01 +0300

    feat: techs

--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -4,7 +4,20 @@ import Info from '../Info/Info';
 const Techs = () => {
   return (
     <Info title="Технологии" linkId="techs" type="techs">
-      <p>skdjskj</p>
+      <div className="techs">
+        <h2 className="techs__title">Технологии</h2>
+        <p className="techs__text">На курсе веб-разработки мы освоили технологии, которые применили
+        в дипломном проекте.</p>
+        <ul className="techs__list">
+          <p className="techs__item">HTML</p>
+          <p className="techs__item">CSS</p>
+          <p className="techs__item">JS</p>
+          <p className="techs__item">React</p>
+          <p className="techs__item">Git</p>
+          <p className="techs__item">Express.js</p>
+          <p className="techs__item">MongoDB</p>
+        </ul>
+      </div>
     </Info>
   );
 };
